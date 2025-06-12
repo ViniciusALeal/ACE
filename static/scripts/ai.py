04acf57d10a68abcd387a23cc88cb33968491c7f
ViniciusALeal <viniciusarmendarisl@gmail.com>
2025-06-12 03:53:52 -0300

    Limpeza de arquivos desnecessários e atualização do ai.py com suporte ao dotenv

--- static/scripts/ai.py
+++ static/scripts/ai.py
@@ -1,17 +1,21 @@
-from openai import OpenAI
+from google import genai
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
+
+
+API_KEY = os.getenv("GOOGLE_API_KEY")
 
 def resposta_llm(pergunta):
-    client = OpenAI(
-        base_url="https://openrouter.ai/api/v1",
-        api_key="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",
-    )
+    if not API_KEY:
+        raise ValueError("Chave da API não encontrada. Verifique o arquivo .env.")
 
-    try:
-        completion = client.chat.completions.create(
-            model="google/gemini-2.5-pro-exp-03-25:free",
-            messages=[{"role": "user", "content": pergunta}]
-        )
-        return completion.choices[0].message.content
-    except Exception as e:
-        return f"Erro ao gerar resposta: {e}"
+    client = genai.Client(api_key=API_KEY)
+
+    response = client.models.generate_content(
+        model="gemini-2.0-flash", contents=pergunta
+    )
+    return response.text
 
